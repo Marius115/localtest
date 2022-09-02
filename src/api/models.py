a2a11a670410269c645efe4edd28dbaf10f37527
Marius115 <cascaranomeister@gmail.com>
2022-09-02 12:20:26 -0400

    Agregado modelos de base de datos, falta actualizar.

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __init__(self,name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.is_active = False      
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +26,87 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.integer, primary_key=True)
+    name = db.Column(db.string(150))
+    gender = db.Column(db.string(150))
+    birth_year = db.Column(db.string(150))
+    height = db.Column(db.string(150))
+
+    def __init__(self,name, gender, birth_year,height):
+        self.name = name
+        self.gender = gender
+        self.birth_year = birth_year
+        self.height = height 
+        db.session.add(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return '<Character %r>' % self.user_id
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "gender":self.gender,
+            "birth_year":self.birth_year,
+            "height":self.height
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'  
+    id = db.Column(db.integer, primary_key=True)
+    name = db.Column(db.string(150))
+    terrain = db.Column(db.string(150))
+    climate = db.Column(db.string(150))
+    population = db.Column(db.string(150))
+
+    def __init__(self,name, terrain, climate,population):
+        self.name = name
+        self.terrain =terrain
+        self.climate = climate
+        self.population = population 
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Planet %r>' % self.user_id
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "terrain":self.terrain,
+            "climate":self.climate,
+            "population":self.population
+
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.integer, primary_key=True)
+    user_id = db.Column(db.integer, db.ForeignKey("user.id"))
+    character_id = db.Column(db.integer, db.ForeignKey("character.id"))
+    planet_id = db.Column(db.integer, db.ForeignKey("planet.id"))
+
+    def delete(self):
+        db.session.delete(self)
+        try:
+            db.session.commit()
+            return True
+        except Exception as error:
+            print(error.args)
+            return False
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id
+        }
+
+    
+    
