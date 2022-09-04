45ee1d221e7b0636e3e785d6c368e36a5cc7a558
Marius115 <cascaranomeister@gmail.com>
2022-09-04 02:50:35 -0400

    :art: añadido el Signup con diseño

--- src/front/js/pages/SignUp.js
+++ src/front/js/pages/SignUp.js
@@ -11,7 +11,104 @@ export const SignUp = (props) => {
 
   return (
     <section className="vh-100" style={{ backgroundColor: "#eee" }}>
-      <div className="container h-100"></div>
+      <div className="container h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-lg-12 col-xl-11">
+            <div className="card text-black" style={{ borderRadius: "25px" }}>
+              <div className="card-body p-md-5">
+                <div className="row justify-content-center">
+                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      Sign up
+                    </p>
+
+                    <form className="mx-1 mx-md-4">
+                      <div className="d-flex flex-row align-items-center mb-4">
+                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                        <div className="form-outline flex-fill mb-0">
+                          <input
+                            type="text"
+                            name="name"
+                            value={name}
+                            placeholder="name"
+                            onChange={(e) => setName(e.target.value)}
+                            className="form-control"
+                          />
+                          <label className="form-label" for="name">
+                            Your Name
+                          </label>
+                        </div>
+                      </div>
+
+                      <div className="d-flex flex-row align-items-center mb-4">
+                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                        <div className="form-outline flex-fill mb-0">
+                          <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            placeholder="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                          />
+                          <label className="form-label" for="email">
+                            Your Email
+                          </label>
+                        </div>
+                      </div>
+
+                      <div className="d-flex flex-row align-items-center mb-4">
+                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <div className="form-outline flex-fill mb-0">
+                          <input
+                            type="password"
+                            name="password"
+                            value={password}
+                            placeholder="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="form-control"
+                          />
+                          <label className="form-label" for="password">
+                            Password
+                          </label>
+                        </div>
+                      </div>
+
+                      <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                        <button
+                          type="button"
+                          className="btn btn-primary btn-lg"
+                          onClick={async (e) => {
+                            const success = await actions.signUp({
+                              name: name,
+                              email: email,
+                              password: password,
+                            });
+                            if (success) {
+                              navigate("/login");
+                              return;
+                            }
+                            alert("something happened while creating");
+                          }}
+                        >
+                          {"Register"}
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img
+                      src="https://i.pinimg.com/originals/92/25/cd/9225cd08ef9c7d6c618befb4e861e7af.jpg"
+                      className="img-fluid h-90"
+                      alt="Sample image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
