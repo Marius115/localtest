e24ca87f7399726dd49027b2199f703c08fdc7b1
Marius115 <cascaranomeister@gmail.com>
2022-09-09 02:26:53 -0400

    cambios esteticos menores

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-3">
+      <div className="container">
+        <Link to="/">
+          <img
+            src="https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo.png"
+            className="w-50 content-center"
+            style={{
+              maxWidth: "600px",
+              maxHeight: "300px",
+            }}
+          />
+        </Link>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
