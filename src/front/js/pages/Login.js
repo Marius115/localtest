93eb42d8d1a2eec2a6f62399004a9c615f57a984
Marius115 <cascaranomeister@gmail.com>
2022-12-10 22:11:58 -0400

    arreglo de Login y Signup

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -9,87 +9,51 @@ export const Login = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#eee" }}>
-      <div className="container h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-lg-12 col-xl-11">
-            <div className="card text-black" style={{ borderRadius: "25px" }}>
-              <div className="card-body p-md-5">
-                <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                      Login
-                    </p>
-
-                    <form className="mx-1 mx-md-4">
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            placeholder="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="email">
-                            Your Email
-                          </label>
-                        </div>
-                      </div>
-
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            name="password"
-                            value={password}
-                            placeholder="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="form-control"
-                          />
-                          <label className="form-label" htmlFor="password">
-                            Password
-                          </label>
-                        </div>
-                      </div>
-
-                      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button
-                          type="button"
-                          className="btn btn-primary btn-lg"
-                          onClick={async (e) => {
-                            const success = await actions.login({
-                              email: email,
-                              password: password,
-                            });
-                            if (success) {
-                              navigate("/");
-                              return;
-                            }
-                            alert("something happened while creating");
-                          }}
-                        >
-                          {"Login"}
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                    <img
-                      src="https://i.imgur.com/gtbuBJV.png"
-                      className="img-fluid h-90"
-                      alt="Sample image"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div
+      className=" d-flex portrait-login align-items-center justify-content-center"
+      style={{ height: "80vh" }}
+    >
+      <div
+        className="w-50 h-75 d-flex fondocaimaneras container flex-column align-items-center justify-content-around bg-opacity-75 py-5"
+        style={{ borderRadius: "25px" }}
+      >
+        <h2 className="text-white">Login De Usuario</h2>
+        <form>
+          <input
+            type="text"
+            name="email"
+            className="form-control my-1"
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            className="form-control my-5"
+            value={password}
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={async (e) => {
+            const success = await actions.login({
+              email: email,
+              password: password,
+            });
+            if (success) {
+              navigate("/");
+              return;
+            }
+            alert("something happened while creating");
+          }}
+        >
+          {"Login"}
+        </button>
       </div>
-    </section>
+    </div>
   );
 };
